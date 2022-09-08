908d3e481772a54eadd76d0f107a38629d718887
zinnetyazicii <zinnetyazicii@gmail.com>
2022-09-09 01:32:21 +0300

    js

---
+++ responsive-modal/app.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+function openPopup(){
+    popup.style.visibility="visible"
+
+    
+}
+function closePopup(){
+    popup.style.visibility="hidden"
+
+}
